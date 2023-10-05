a137cdca83e87f3ae50d653542af876e37074730
Makei Arina <makejarina@gmail.com>
2023-10-05 11:05:47 +0300

    тестовое сохранение111

--- ЛР7/зад1.py
+++ ЛР7/зад1.py
@@ -1,2 +1,7 @@
-
-print('test')
+func = ('abs', 'round', 'min', 'max', 'sum', 'pow', 'divmod')#Создаём кортеж
+nuw_tu=func[0:3]#Первые три элемента присваиваем nuw_tu
+print(nuw_tu)#Выводим на экран результат
+nuw_tu2=func[3:7]#Оставшиеся четыре элемента присваиваем new_tu2
+print(nuw_tu2)#Выводим на экран результат
+all_tu=nuw_tu+nuw_tu2#Присваиваем все значения new_tu и new_tu2 для all_tu
+print(all_tu)#Выводим на экран результат
