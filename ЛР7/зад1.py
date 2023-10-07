58c81db181deb9a55230a08c3b4038f476764da2
Makei Arina <makejarina@gmail.com>
2023-10-07 19:01:09 +0300

    второе задание

--- ЛР7/зад1.py
+++ ЛР7/зад1.py
@@ -1,7 +1,7 @@
-func = ('abs', 'round', 'min', 'max', 'sum', 'pow', 'divmod')#Создаём кортеж
-nuw_tu=func[0:3]#Первые три элемента присваиваем nuw_tu
+function = ( 'round', 'divmod', 'sum', 'float', 'max','min','abs')#Создаём кортеж
+nuw_tu=function[0:3]#Первые три элемента присваиваем nuw_tu
 print(nuw_tu)#Выводим на экран результат
-nuw_tu2=func[3:7]#Оставшиеся четыре элемента присваиваем new_tu2
+nuw_tu2=function[3:7]#Оставшиеся четыре элемента присваиваем new_tu2
 print(nuw_tu2)#Выводим на экран результат
 all_tu=nuw_tu+nuw_tu2#Присваиваем все значения new_tu и new_tu2 для all_tu
 print(all_tu)#Выводим на экран результат
